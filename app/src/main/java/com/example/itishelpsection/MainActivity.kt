6943fb3bdfba21249dc3b84698ec1963a204e469
Dorritos <dorritos002@gmail.com>
2020-10-01 08:41:06 +0300

    я в ахуе

--- app/src/main/java/com/example/itishelpsection/MainActivity.kt
+++ app/src/main/java/com/example/itishelpsection/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.itishelpsection
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewWebSite = findViewById(R.id.textViewWebSite) as TextView
+
+        textViewWebSite.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kamaz.ru"))
+            startActivity(browserIntent)
+        }
     }
 }
